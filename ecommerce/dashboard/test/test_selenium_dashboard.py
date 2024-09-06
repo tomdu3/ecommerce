e64e7875a013860019d0f89470e112ef52504d34
Tomislav Dukez <tomdu3@ymail.com>
2024-09-06 14:19:49 +0200

    add admin login test

--- ecommerce/dashboard/test/test_selenium_dashboard.py
+++ ecommerce/dashboard/test/test_selenium_dashboard.py
@@ -1,2 +1,24 @@
-def test_example():
-    assert "Hello" == "Hello"
+import pytest
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+@pytest.mark.selenium
+def test_create_new_admin_user(create_admin_user):
+    assert create_admin_user.__str__() == "admin"
+
+
+@pytest.mark.selenium
+def test_dashboard_admin_login(live_server, create_admin_user, chrome_browser_instance):
+    browser = chrome_browser_instance
+    browser.get("%s%s" % (live_server.url, "/admin/login/"))
+
+    user_name = browser.find_element(By.NAME, "username")
+    user_password = browser.find_element(By.NAME, "password")
+    submit = browser.find_element(By.XPATH, '//input[@value="Log in"]')
+
+    user_name.send_keys("admin")
+    user_password.send_keys("admin")
+    submit.send_keys(Keys.RETURN)
+
+    assert "Django administration" in browser.page_source
