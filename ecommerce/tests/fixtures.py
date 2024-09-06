f3fc87fce59998838e27b094a244a9e695186c91
Tomislav Dukez <tomdu3@ymail.com>
2024-09-06 15:36:04 +0200

    add fixtures

--- ecommerce/tests/fixtures.py
+++ ecommerce/tests/fixtures.py
@@ -1,4 +1,6 @@
 import pytest
+from django.contrib.auth.models import User
+from django.core.management import call_command
 
 
 @pytest.fixture()
@@ -13,3 +15,12 @@ def create_admin_user(django_user_model):
         username=username, email=email, password=password
     )
     return user
+
+
+@pytest.fixture(scope="session")
+def django_fixture_setup(django_db_setup, django_db_blocker):
+    """
+    Load database data fixtures
+    """
+    with django_db_blocker.unblock():
+        call_command("loaddata", "db_admin_fixture.json")
