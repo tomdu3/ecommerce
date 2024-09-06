a88d4cf30636f947bb878430db69fd02a2935dcb
Tomislav Dukez <tomdu3@ymail.com>
2024-09-06 15:37:27 +0200

    modify selenium test

--- ecommerce/dashboard/tests/test_selenium_dashboard.py
+++ ecommerce/dashboard/tests/test_selenium_dashboard.py
@@ -3,13 +3,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
-@pytest.mark.selenium
-def test_create_new_admin_user(create_admin_user):
-    assert create_admin_user.__str__() == "admin"
+# @pytest.mark.selenium
+# def test_create_new_admin_user(create_admin_user):
+#    assert create_admin_user.__str__() == "admin"
 
 
 @pytest.mark.selenium
-def test_dashboard_admin_login(live_server, create_admin_user, chrome_browser_instance):
+def test_dashboard_admin_login(live_server, db_fixture_setup, chrome_browser_instance):
     browser = chrome_browser_instance
     browser.get("%s%s" % (live_server.url, "/admin/login/"))
 
