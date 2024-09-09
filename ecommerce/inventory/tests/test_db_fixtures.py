aea7693627011d6a8af9f2a1081dc12c52c22351
Tomislav Dukez <tomdu3@ymail.com>
2024-09-09 22:01:59 +0200

    add test db fixtures in inventory

---
+++ ecommerce/inventory/tests/test_db_fixtures.py
@@ -0,0 +1,21 @@
+import pytest
+from ecommerce.inventory import models
+
+
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    "id, name, slug, is_active",
+        [
+            (1, "fashion", "fashion", True),
+            (18, "electronics", "electronics", True),
+            (27, "home", "home", True),
+        ],
+)
+
+def test_inventory_category_dbfixture(
+        db. db_fixture_setup, id, name, slug, is_active
+):
+    result = models.Category.objects.get(id=id)
+    assert result.name == name
+    assert result.slug == slug
+    assert result.is_active == is_active
