9e157cf8c24b4cd05e73374387176cf4db216cd6
Tomislav Dukez <tomdu3@ymail.com>
2025-01-05 08:02:38 +0000

    add test db inventory product table and fixtures

--- ecommerce/inventory/tests/test_db_fixtures.py
+++ ecommerce/inventory/tests/test_db_fixtures.py
@@ -1,6 +1,10 @@
 import pytest
 from ecommerce.inventory import models
 
+# fixture test for inventory.category table
+
+# manual fixture creation
+
 
 @pytest.mark.dbfixture
 @pytest.mark.parametrize(
@@ -19,7 +23,9 @@ def test_inventory_category_dbfixture(
     assert result.slug == slug
     assert result.is_active == is_active
 
-# factory boy
+# factory boy fixture creation
+
+
 @pytest.mark.parametrize(
     "name, slug, is_active",
     [
@@ -28,9 +34,72 @@ def test_inventory_category_dbfixture(
         ("home", "home", True),
     ],
 )
+# testing of the inventory.category table
+def test_inventory_db_category_insert_data(
+    db,
+    category_factory,
+    name,
+    slug,
+    is_active,
+):
 
-def test_inventory_db_category_insert_data(db, category_factory, name, slug, is_active):
     result = category_factory.create(name=name, slug=slug, is_active=is_active)
     assert result.name == name
     assert result.slug == slug
     assert result.is_active == is_active
+
+
+# Testing of the inventory.product table
+
+
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    # we are excluding category field
+    "id, web_id, name, slug, description, is_active, created_at, updated_at",
+    [
+        (
+            1,
+            "45425810",
+            "widstar running sneakers",
+            "widstar-running-sneakers",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porta, eros vel sollicitudin lacinia, quam metus gravida elit, a elementum nisl neque sit amet orci. Nulla id lorem ac nunc cursus consequat vitae ut orci. In a velit eu justo eleifend tincidunt vel eu turpis. Praesent eu orci egestas, lobortis magna egestas, tincidunt augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean vitae lectus eget tortor laoreet efficitur vel et leo. Maecenas volutpat eget ante id tempor. Etiam posuere ex urna, at aliquet risus tempor eu. Aenean a odio odio. Nunc consectetur lorem ante, interdum ultrices elit consectetur sit amet. Vestibulum rutrum interdum nulla. Cras vel mi a enim eleifend blandit. Curabitur ex dui, rutrum et odio sit amet, auctor euismod massa.",
+            1,  # True
+            "2021-09-04 22:14:18",
+                "2021-09-04 22:14:18",
+        ),
+        (
+            8616,
+            "45434425",
+            "impact puse dance shoe",
+            "impact-puse-dance-shoe",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porta, eros vel sollicitudin lacinia, quam metus gravida elit, a elementum nisl neque sit amet orci. Nulla id lorem ac nunc cursus consequat vitae ut orci. In a velit eu justo eleifend tincidunt vel eu turpis. Praesent eu orci egestas, lobortis magna egestas, tincidunt augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean vitae lectus eget tortor laoreet efficitur vel et leo. Maecenas volutpat eget ante id tempor. Etiam posuere ex urna, at aliquet risus tempor eu. Aenean a odio odio. Nunc consectetur lorem ante, interdum ultrices elit consectetur sit amet. Vestibulum rutrum interdum nulla. Cras vel mi a enim eleifend blandit. Curabitur ex dui, rutrum et odio sit amet, auctor euismod massa.",
+            1,  # True
+            "2021-09-04 22:14:18",
+                "2021-09-04 22:14:18",
+        ),
+    ],
+)
+def test_inventory_db_product_dbfixture(
+    db,
+    db_fixture_setup,
+    id,
+    web_id,
+    name,
+    slug,
+    description,
+    is_active,
+    created_at,
+    updated_at,
+):
+
+    result = models.Product.objects.get(id=id)
+    result_create_at = result.created_at.strftime("%Y-%m-%d %H:%M:%S")
+    result_updated_at = result.updated_at.strftime("%Y-%m-%d %H:%M:%S")
+
+    assert result.web_id == web_id
+    assert result.name == name
+    assert result.slug == slug
+    assert result.description == description
+    assert result.is_active == is_active
+    assert result_updated_at == created_at
+    assert result_updated_at == updated_at
